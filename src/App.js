c2023151c73b46f4ee50d1b96d24e23503849585
phat.tran <phat.tran@globee.hk>
2022-03-16 10:24:31 +0700

    - Update view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useRef } from 'react';
 import Task from './components/Task';
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Grid, Paper, Snackbar, Alert } from '@mui/material';
 
 import './styles/card.scss';
 
@@ -10,6 +10,11 @@ function App() {
     const [tasks, updateTasks] = useState(() => JSON.parse(localStorage.getItem('tasks')) ?? []);
     const [error, setError] = useState(false);
     const inputRef = useRef();
+    const containerStyle = {
+        maxWidth: '400px',
+        margin: '10px auto',
+        padding: '20px 50px'
+    };
     const addNewTask = () => {
         if (!task) {
             setError(true);
@@ -33,33 +38,53 @@ function App() {
         inputRef.current.focus();
         localStorage.setItem('nextIndex', index + 2);
     }
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setError(false);
+    };
+    const vertical = 'top';
+    const horizontal = 'left';
 
     return (
-        <div className='App'>
-            <div className='form'>
-                <TextField id='outlined-basic'
-                    label='Task'
-                    variant='outlined'
-                    size='small'
-                    error={error}
-                    value={task}
-                    inputRef={inputRef}
-                    onChange={(e) => updateTask(e.target.value)}
-                    onKeyDown={(e) => {
-                        (e.key === 'Enter') && addNewTask();
-                    }} />
-                <Button onClick={addNewTask} variant='contained'>Enter Task</Button>
-            </div>
-            {
-                tasks.map((task) => {
-                    return (
-                        <div className='task-element' key={task.id}>
-                            <Task id={task.id} title={task.title} completed={task.isCompleted} />
-                        </div>
-                    )
-                })
-            }
-        </div>
+        <Grid>
+            <Snackbar open={error} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical, horizontal }}>
+                <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                    Please input task name!
+                </Alert>
+            </Snackbar>
+            <Paper elevation={20} className='App' style={containerStyle}>
+                <Grid className='form' container style={{ marginBottom: '20px' }}>
+                    <Grid xs={8}>
+                        <TextField id='outlined-basic'
+                            label='Task'
+                            variant='outlined'
+                            size='small'
+                            error={error}
+                            value={task}
+                            inputRef={inputRef}
+                            onChange={(e) => updateTask(e.target.value)}
+                            onKeyDown={(e) => {
+                                (e.key === 'Enter') && addNewTask();
+                            }} />
+                    </Grid>
+                    <Grid xs={4}>
+                        <Button onClick={addNewTask} variant='contained'>Enter Task</Button>
+                    </Grid>
+                </Grid>
+                {
+                    tasks.map((task) => {
+                        return (
+                            <div className='task-element' key={task.id}>
+                                <Task id={task.id} title={task.title} completed={task.isCompleted} />
+                            </div>
+                        )
+                    })
+                }
+            </Paper>
+        </Grid>
     );
 }
 
