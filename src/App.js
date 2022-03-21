deef2917d22966966c326bec498d1aeeb3bfc376
phat.tran <phat.tran@globee.hk>
2022-03-21 13:07:59 +0700

    - Apply dialog

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useRef } from 'react';
-import { Button, TextField, Grid, Paper, Snackbar, Alert } from '@mui/material';
+import { Button, TextField, Grid, Paper, Snackbar, Alert, Dialog, DialogContent } from '@mui/material';
 import { v4 as uuid } from 'uuid';
 import { useDispatch, useSelector } from 'react-redux';
 import { addTodo } from './redux/actions';
@@ -15,13 +15,15 @@ function App() {
     const inputRef = useRef();
     const dispatch = useDispatch();
     const containerStyle = {
-        maxWidth: '400px',
+        maxWidth: '500px',
+        minWidth: '380px',
         margin: '10px auto',
         padding: '20px 50px'
     };
     const addNewTask = () => {
         if (!task) {
             setError(true);
+            inputRef.current.focus();
             return;
         };
 
@@ -45,44 +47,62 @@ function App() {
     };
     const vertical = 'top';
     const horizontal = 'left';
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpen(false);
+    };
 
     return (
-        <Grid>
-            <Snackbar open={error} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical, horizontal }}>
-                <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
-                    Please input task name!
-                </Alert>
-            </Snackbar>
-            <Paper elevation={20} className='App' style={containerStyle}>
-                <Grid className='form' container style={{ marginBottom: '20px' }}>
-                    <Grid item xs={8}>
-                        <TextField id='outlined-basic'
-                            label='Task'
-                            variant='outlined'
-                            size='small'
-                            error={error}
-                            value={task}
-                            inputRef={inputRef}
-                            onChange={(e) => updateTask(e.target.value)}
-                            onKeyDown={(e) => {
-                                (e.key === 'Enter') && addNewTask();
-                            }} />
+        <div>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                Open
+            </Button>
+            <Dialog open={open} TransitionProps={{ onEntering: () => inputRef.current.focus()}} onClose={handleCloseDialog}>
+                <DialogContent>
+                    <Grid>
+                        <Snackbar open={error} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical, horizontal }}>
+                            <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                                Please input task name!
+                            </Alert>
+                        </Snackbar>
+                        <Paper elevation={0} className='App' style={containerStyle}>
+                            <Grid className='form' container style={{ marginBottom: '2rem'}}>
+                                <Grid item xs={8}>
+                                    <TextField id='outlined-basic'
+                                        label='Task'
+                                        variant='outlined'
+                                        size='small'
+                                        error={error}
+                                        value={task}
+                                        inputRef={inputRef}
+                                        onChange={(e) => updateTask(e.target.value)}
+                                        onKeyDown={(e) => {
+                                            (e.key === 'Enter') && addNewTask();
+                                        }} />
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Button onClick={addNewTask} variant='contained'>Enter Task</Button>
+                                </Grid>
+                            </Grid>
+                            {
+                                tasks.map((task) => {
+                                    return (
+                                        <div className='task-element' key={task.id}>
+                                            <Task id={task.id} title={task.title} completed={task.isCompleted} />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </Paper>
                     </Grid>
-                    <Grid item xs={4}>
-                        <Button onClick={addNewTask} variant='contained'>Enter Task</Button>
-                    </Grid>
-                </Grid>
-                {
-                    tasks.map((task) => {
-                        return (
-                            <div className='task-element' key={task.id}>
-                                <Task id={task.id} title={task.title} completed={task.isCompleted} />
-                            </div>
-                        )
-                    })
-                }
-            </Paper>
-        </Grid>
+                </DialogContent>
+            </Dialog>
+        </div>
     );
 }
 
